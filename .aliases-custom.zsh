8094fe93a448f3496bdb4810751aa151a730262d
Mason Garcia <43070886+MasonMasoff@users.noreply.github.com>
2025-09-28 18:03:23 -0500

    Custom Aliases

---
+++ .aliases-custom.zsh
@@ -0,0 +1,21 @@
+# Custom aliases
+# Add your personal aliases, overriding those provided by the system or other plugins
+
+# Example aliases
+alias reload="source ~/.zshrc"
+alias dotfiles="cd ~/.dotfiles-linux"
+
+# Git aliases
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git pull"
+
+# Directory navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+# Add your own custom aliases below
+# -------------------------------------
